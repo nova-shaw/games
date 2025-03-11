78c18448797fce89cd7daf1f37b31d7cb41d7506
nova-shaw <197215180+nova-shaw@users.noreply.github.com>
2025-03-11 15:59:05 +0900

    Zoom game basics, interface needed

--- zoom/game-zoom.js
+++ zoom/game-zoom.js
@@ -1,4 +1,5 @@
 
+import { uiElement } from '../_common/modules/ui-element.js'
 import * as timer from '../_common/modules/timer.js'
 import * as lessonData from '../_lessons/class5_bookb_07.js';
 
@@ -16,17 +17,9 @@ const cardList = [];
 
 
 function buildDeck(data) {
-  const deck = document.createElement('div');
-  deck.id = 'deck';
-
-  data.vocab.forEach( item => {
-    const card = buildCard(item);
-    cardList.push(card);
-    deck.appendChild(card);
-  });
-
-  // log(cardList);
-
+  const deck = uiElement({ type: 'div', id: 'deck' });
+  const card = buildCard(data.vocab[4]);
+  deck.appendChild(card);
   display.appendChild(deck);
 }
 
@@ -34,31 +27,16 @@ function buildDeck(data) {
 
 function buildCard(cardData) {
 
-  const card  = document.createElement('div');
-  card.classList.add('card');
-
-  const sides = document.createElement('div');
-  sides.classList.add('sides');
-  const back  = document.createElement('div');
-  back.classList.add('back');
-  const face = document.createElement('div');
-  face.classList.add('face');
-
-  const img = document.createElement('img');
-  img.src = `${mediaPath}${cardData.image}`;
-
-  const text = document.createElement('p');
-  text.classList.add('text');
-  text.textContent = cardData.text;
+  const card = uiElement({ type: 'div', classes: 'card' });
+  const face = uiElement({ type: 'div', classes: 'face' });
+  const img  = uiElement({ type: 'img', attrs: { src: `${mediaPath}${cardData.image}` } });
+  const text = uiElement({ type: 'p', classes: 'text', text: cardData.text });
 
   face.appendChild(img);
   face.appendChild(text);
+  card.appendChild(face);
 
-  sides.appendChild(face);
-  sides.appendChild(back);
-  card.appendChild(sides);
-
-  card.addEventListener('click', flipCard);
+  card.addEventListener('click', startZoom);
 
   return card;
 }
@@ -66,9 +44,9 @@ function buildCard(cardData) {
 buildDeck(lessonData.data);
 
 
-function flipCard(e) {
+function startZoom(e) {
   const card = e.currentTarget;
-  card.classList.toggle('show');
+  card.classList.toggle('zoom');
 }
 
 
@@ -76,73 +54,3 @@ function flipCard(e) {
 
 //// Play
 
-
-const btnPlay = document.querySelector('button#play');
-btnPlay.addEventListener('click', () => {
-  const playingTrue = display.classList.toggle('playing'); //// toggle() returns boolean
-  if (playingTrue) {
-    timer.play();
-  } else {
-    timer.pause();
-  }
-});
-
-const rngSpeed = document.querySelector('#speed');
-rngSpeed.addEventListener('input', (e) => {
-  const val = e.currentTarget.value;
-  timer.intervalSet( speedToInterval(val) );
-
-  const gapMS = speedToGap(val);
-  timer.gapSet( gapMS );
-  document.documentElement.style.setProperty('--flipms', `${gapMS * 0.8}ms`);
-});
-
-const fwd = document.querySelector('button#fwd');
-fwd.addEventListener('click', e => {
-  timer.settings({ playBackwards: false });
-});
-
-const bck = document.querySelector('button#bck');
-bck.addEventListener('click', e => {
-  timer.settings({ playBackwards: true });
-});
-
-const rnd = document.querySelector('#playrandom');
-rnd.addEventListener('change', e => {
-  timer.settings({ playRandom: e.currentTarget.checked });
-});
-
-
-timer.settings({
-  interval: speedToInterval(rngSpeed.value),
-  intervalGap: 1000,
-  maxIndex: cardList.length - 1,
-  intervalStartCallback: showCard,
-  intervalEndCallback: hideCard,
-  // playBackwards: true,
-  playRandom: true
-});
-
-
-
-
-function showCard(index) {
-  // log('show', index)
-  cardList[index].classList.add('show');
-}
-
-function hideCard(index) {
-  // log('hide', index)
-  cardList[index].classList.remove('show');
-}
-
-
-//// Utils
-
-function speedToInterval(value) {
-  return 2400 - (value * 20);
-}
-
-function speedToGap(value) {
-  return 800 - (value * 5);
-}

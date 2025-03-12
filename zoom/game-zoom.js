d6d6221ae453581c1e6673cf77980da578ac90e4
Shaw <2610597+shawneither@users.noreply.github.com>
2025-03-12 14:50:39 +0900

    Animation code extracted to module, main Zoom js cleaned up

--- zoom/game-zoom.js
+++ zoom/game-zoom.js
@@ -1,6 +1,6 @@
-import { lerp, norm, easeInQuart, easeOutQuart, easeInCubic, easeOutQuad, easeOutCubic } from '../_common/modules/utils.js';
+import { lerp, easeInQuart } from '../_common/modules/utils.js';
 import { uiElement } from '../_common/modules/ui-element.js';
-import * as timer from '../_common/modules/timer.js';
+import * as anim from '../_common/modules/anim.js';
 import * as lessonData from '../_lessons/class5_bookb_07.js';
 
 const log = console.log;
@@ -15,24 +15,17 @@ let card = null;
 let face = null;
 let text = null;
 
-const cardList = [];
-
 
 function buildDeck(data) {
   const deck = uiElement({ type: 'div', id: 'deck' });
   
   const cardChooser = document.querySelector('ul#card-chooser');
-
   data.vocab.forEach( item => {
     const cardThumb = buildCardThumb(item);
-    // cardList.push(cardThumb);
     cardChooser.appendChild(cardThumb);
   });
 
-  // const card = buildCard(data.vocab[4]);
-  // const card = buildBlankCard().card;
   card = buildCard();
-  // card.classList.add('blank');
   deck.appendChild(card);
   display.appendChild(deck);
 }
@@ -57,27 +50,18 @@ function buildCardThumb(cardData) {
 function chooseCard(e) {
   const cardSlug = e.currentTarget.dataset.card;
   const cardData = lessonData.data.vocab.find( item => item.slug == cardSlug );
-  // log(cardData);
-  // log(cardSlug);
-  
-  /*const newCard = buildCard(cardData);
-  newCard.classList.add('zoom')
-  const oldCard = document.querySelector('#deck .card');
-  oldCard.replaceWith(newCard);
-  card = newCard;*/
-
 
   const face = buildCardFace(cardData);
   const currentface = card.querySelector('.face');
   text = face.querySelector('.text');
   currentface.replaceWith(face);
 
+  const poi = (cardData.poi) ? cardData.poi : [50, 50]; // Fallback to center if no 'Point Of Interest' found in image data
+  card.style.setProperty('--poi', `${poi[0]}% ${poi[1]}%`);
 
-  const poi = (cardData.poi) ? cardData.poi : [50, 50];
-  card.style.setProperty('--poi', `${poi[0]}% ${poi[1]}%`)
   card.style.setProperty(`--zoom`, maxZoom);
 
-  animReset();
+  anim.reset();
 }
 
 
@@ -91,28 +75,21 @@ function buildCard(cardData) {
   return card;
 }
 
-// function buildBlankCard() {
-//   const card = uiElement({ type: 'div', classes: 'card' });
-//   const face = uiElement({ type: 'div', classes: 'face blank' });
-//   card.appendChild(face);
-//   return card;
-// }
-
 function buildCardFace(cardData = null) {
+
   const face = uiElement({ type: 'div', classes: 'face' });
+
   if (cardData) {
     const img  = uiElement({ type: 'img', attrs: { src: `${mediaPath}${cardData.image}` } });
     const text = uiElement({ type: 'p', classes: 'text', text: cardData.text });
     face.appendChild(img);
     face.appendChild(text);
+
   } else {
     face.classList.add('blank');
   }
   return face;
 }
-
-
-
 
 buildDeck(lessonData.data);
 
@@ -126,15 +103,12 @@ buildDeck(lessonData.data);
 const maxZoom = 40;
 const minZoom = 1;
 
-function doTheAnimation(per) { // `per` is float 0-1
+function zoomOutSlowly(per) { // `per` is float 0-1
   const factor = easeInQuart(1 - per); // Invert the percent and ease
-  // const val = (maxZoom * factor) + minZoom;
   const val = lerp(factor, minZoom, maxZoom);
   card.style.setProperty(`--zoom`, val);
   text?.classList.toggle('show', per > 0.98);
 }
-
-
 
 
 
@@ -145,108 +119,9 @@ const animToggle = document.querySelector('#btn_playtoggle');
 const animRange  = document.querySelector('#rng_playback');
 const animReveal = document.querySelector('#btn_reveal');
 
-animToggle.addEventListener('click', () => {
-  if (playing) {
-    animPause();
-  } else {
-    animPlay();
-  }
-});
-
-animRange.addEventListener('pointerdown', e => {
-  playOnRelease = playing;
-  playing = false;
-  // animJump(e.currentTarget.value);
-  animJumpFromRange(e);
-});
-
-animRange.addEventListener('input', e => {
-  // animJump(e.currentTarget.value);
-  animJumpFromRange(e);
-});
-
-animRange.addEventListener('pointerup', e => {
-  if (playOnRelease) animPlay();
-});
+anim.setup(animToggle, animRange, 10000, zoomOutSlowly);
 
 animReveal.addEventListener('click', e => {
-  // duration = 1000;
-  animPlay(1000);
-})
+  anim.play(1000);
+});
 
-
-///////////////////////////////////////////////////////////////
-// Animation internals
-
-const durationMain = 10000;
-// let durationOnce = null;
-
-let duration = durationMain;
-let playing = false;
-let elapsed = 0;
-let startAt = 0;
-let zero = 0;
-let playOnRelease = false;
-
-function animLoop(timestamp) {
-
-  if (!playing) return;
-
-  requestAnimationFrame(animLoop);
-
-  elapsed = startAt + (timestamp - zero);
-
-  const per = (elapsed / duration);
-  animRange.value = per;
-
-  doTheAnimation(per);
-
-  if (elapsed >= duration) {
-    // elapsed = duration;
-    animEnd();
-    // animReset();
-  }
-}
-
-function animPlay(durationOnce = null) {
-  if (durationOnce) {
-    startAt = durationOnce * (elapsed / duration);
-    duration = durationOnce;
-  }
-  playing = true;
-  zero = document.timeline.currentTime;
-  animLoop(zero);
-}
-
-function animPause() {
-  duration = durationMain;
-  startAt = elapsed;
-  playing = false;
-}
-
-function animEnd() {
-  duration = durationMain;
-  elapsed = 0;
-  animRange.value = 1;
-  doTheAnimation(1);
-  playing = false;
-}
-
-function animReset() {
-  duration = durationMain;
-  startAt = 0;
-  // elapsed = 0;
-  animRange.value = 0;
-  playing = false;
-}
-
-function animJump(per) {
-  startAt = lerp(per, 0, duration);
-  animRange.value = per;
-  doTheAnimation(per);
-}
-
-function animJumpFromRange(e) { // Same as `animJump` but doesn't set UI range value
-  startAt = lerp(e.currentTarget.value, 0, duration);
-  doTheAnimation(e.currentTarget.value);
-}
